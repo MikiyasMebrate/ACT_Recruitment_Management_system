1bbc42f7f471948e0283fae5daf18474cb644565
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2023-06-10 17:27:17 +0300

    First Commit

--- ACT/asgi.py
+++ ACT/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Recruitment_Management_System project.
+ASGI config for ACT project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recruitment_Management_System.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ACT.settings')
 
 application = get_asgi_application()
--- ACT/urls.py
+++ ACT/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Recruitment_Management_System project.
+URL configuration for ACT project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
